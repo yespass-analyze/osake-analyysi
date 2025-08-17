a8e910837ebfe43a9d759ae8de57d2ba54ff952e
yespass-analyze <tommi.juusola@gmail.com>
2025-08-17 13:32:43 +0300

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,1 +1,15 @@
+@echo off
+REM Mene repoon juureen
+cd /d "%~dp0"
+
+REM Päivitä koodi
+git pull origin main
+
+REM Varmista riippuvuudet (vain ensimmäisellä kerralla voit kommentoida pois)
+pip install -r requirements.txt
+
+REM Käynnistä streamlit
+streamlit run app.py
+
+pause
 
